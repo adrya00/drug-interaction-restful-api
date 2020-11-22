828b6973a6fc02eb54d5dc624221c35998c79ef6
Garcia McGee <agarciamcge@chsmedical.com>
2020-11-22 11:02:17 -0700

    first commit. Can see data on command line.

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Store} from './Store';
+
+
+export default function App(){
+
+  const {state, dispatch} = React.useContext(Store);
+  const fetchDataAction = async () => {
+    const data = await fetch('https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=1092422');
+    const dataJSON = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      interRxcuiPayload: dataJSON.interactionTypeGroup[0].interactionType[0].interactionPair[0].interactionConcept[1].minConceptItem.rxcui,
+      namePayload:dataJSON.interactionTypeGroup[0].interactionType[0].interactionPair[0].interactionConcept[1].minConceptItem.name,
+      severityPayload:dataJSON.interactionTypeGroup[0].interactionType[0].interactionPair[0].severity,
+      descPayload:dataJSON.interactionTypeGroup[0].interactionType[0].interactionPair[0].description,
+      urlPayload:dataJSON.interactionTypeGroup[0].interactionType[0].interactionPair[0].interactionConcept[1].sourceConceptItem.url
+    });
+  };
+
+  React.useEffect(() => {
+    state.rxcui.length === 0 && fetchDataAction();
+    
+  });
+
+  return(
+    <React.Fragment>
+      {console.log(state)}
+      <div>
+        <h1>Drug Interactions</h1>
+        <p></p>
+
+      </div>
+    </React.Fragment>
+  );
+}
---
+++ src/Store.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export const Store = React.createContext();
+
+const initialState = {
+
+    rxcui:[],
+    Name:[],
+    interRxcui:[],
+    interName:[],
+    severity:[],
+    description:[],
+    url:[]
+}
+
+function reducer(state, action){
+    switch (action.type){
+        case 'FETCH_DATA':
+            return {...state, interRxcui: action.interRxcuiPayload, interName: action.namePayload, severity: action.severityPayload, 
+                description: action.descPayload, url:action.urlPayload};
+        default:
+            return state;
+    }
+}
+
+export function StoreProvider(props){
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    const value = {state, dispatch};
+return <Store.Provider value={value}>{props.children}
+</Store.Provider>
+}
